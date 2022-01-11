1cd055bc21bc7e47f8734965e178fae78db44bb1
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-11 12:39:31 -0500

    created outlets

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -6,25 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSignIn(_ sender: Any) {
     }
-    */
-
+    
+   
+    @IBAction func onSignup(_ sender: Any) {
+    }
+    
 }
